463436de6a7d96f614b5c39d84565281e6ff13cd
CHAE <chaso1495@naver.com>
2023-07-13 15:40:54 +0900

    [init] first commit

---
+++ src/main/java/com/kusher/kusher_in_korea/KusherInKoreaApplication.java
@@ -0,0 +1,13 @@
+package com.kusher.kusher_in_korea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KusherInKoreaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KusherInKoreaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kusher/kusher_in_korea/KusherInKoreaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kusher.kusher_in_korea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KusherInKoreaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
