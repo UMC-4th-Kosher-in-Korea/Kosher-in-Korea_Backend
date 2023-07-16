bc8b817e16891b97dbbb03494fb407341935d579
CHAE <chaso1495@naver.com>
2023-07-17 00:12:46 +0900

    [feat] #3 User domain 구현

--- src/main/java/com/kusher/kusher_in_korea/tabling/domain/User.java
+++ src/main/java/com/kusher/kusher_in_korea/tabling/domain/User.java
@@ -1,4 +1,41 @@
 package com.kusher.kusher_in_korea.tabling.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "user")
 public class User {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id; // 유저번호
+
+    private String userName; // 유저이름
+
+    private String userPhone; // 전화번호
+
+    private String userType; // 유저유형 (일반유저 or 식당주인)
+
+    // 생성 메서드
+    public static User createUser(String userName, String userPhone, String userType) {
+        User user = new User();
+        user.setUserName(userName);
+        user.setUserPhone(userPhone);
+        user.setUserType(userType);
+        return user;
+    }
+
+    // 비즈니스 로직
+    // 유저 정보 수정
+    public void updateUser(String userName, String userPhone, String userType) {
+        this.userName = userName;
+        this.userPhone = userPhone;
+        this.userType = userType;
+    }
+
 }
