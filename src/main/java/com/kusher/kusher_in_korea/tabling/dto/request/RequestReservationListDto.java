969db4d75ad3aaf6c051ea85ca5899a602e31805
CHAE <chaso1495@naver.com>
2023-07-17 23:27:46 +0900

    [feat] #7 예약 내역 요청 Dto 추가

---
+++ src/main/java/com/kusher/kusher_in_korea/tabling/dto/request/RequestReservationListDto.java
@@ -0,0 +1,10 @@
+package com.kusher.kusher_in_korea.tabling.dto.request;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class RequestReservationListDto {
+    private Long userId; // 유저번호
+}
