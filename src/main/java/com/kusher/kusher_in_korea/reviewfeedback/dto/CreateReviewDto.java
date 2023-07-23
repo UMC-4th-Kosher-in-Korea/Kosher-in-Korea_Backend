7dadd8961440bc9465da41328b9ccea4ef159fe8
CHAE <chaso1495@naver.com>
2023-07-23 21:45:12 +0900

    [feat] #16 평가 및 피드백 createDto 추가

---
+++ src/main/java/com/kusher/kusher_in_korea/reviewfeedback/dto/CreateReviewDto.java
@@ -0,0 +1,14 @@
+package com.kusher.kusher_in_korea.reviewfeedback.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class CreateReviewDto { // 평가 생성 요청
+    private Long userId; // 유저번호
+    private Long restaurantId; // 식당번호
+    private String reviewContent; // 리뷰내용
+    private String reviewTime; // 평가작성일시 -> LocalDateTime으로 변경될 수도 있음
+    private Long reviewScore; // 평점
+}
