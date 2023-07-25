33c08f8a9dce6113797af444872f2d1c6e7d7dfa
CHAE <chaso1495@naver.com>
2023-07-25 17:15:00 +0900

    [feat] #18 식재료/주문 관련 repository 구현

---
+++ src/main/java/com/kusher/kusher_in_korea/ingredient/repository/CategoryRepository.java
@@ -0,0 +1,21 @@
+package com.kusher.kusher_in_korea.ingredient.repository;
+
+import com.kusher.kusher_in_korea.ingredient.domain.Category;
+import org.springframework.data.domain.Example;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+    // 카테고리 삭제
+    void deleteById(Long id);
+
+    // 전체 카테고리 조회
+    List<Category> findAll();
+
+    Optional<Category> findById(Long id);
+
+    // 이름으로 카테고리 조회
+    Optional<Category> findByName(String name);
+}
---
+++ src/main/java/com/kusher/kusher_in_korea/ingredient/repository/OrdersRepository.java
@@ -0,0 +1,12 @@
+package com.kusher.kusher_in_korea.ingredient.repository;
+
+import com.kusher.kusher_in_korea.ingredient.domain.Orders;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface OrdersRepository extends JpaRepository<Orders, Long> {
+
+    // 특정 user의 주문 조회
+    List<Orders> findByUserId(Long userId);
+}
