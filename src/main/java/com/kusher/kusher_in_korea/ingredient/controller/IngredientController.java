ff4855f231e3daf418d478b564a0d282cae0e89e
CHAE <chaso1495@naver.com>
2023-07-26 20:56:07 +0900

    [feat] #18 식재료 Service, Controller Dto 적용

--- src/main/java/com/kusher/kusher_in_korea/ingredient/controller/IngredientController.java
+++ src/main/java/com/kusher/kusher_in_korea/ingredient/controller/IngredientController.java
@@ -1,10 +1,15 @@
 package com.kusher.kusher_in_korea.ingredient.controller;
 
+import com.kusher.kusher_in_korea.ingredient.dto.request.RequestCategoryDto;
+import com.kusher.kusher_in_korea.ingredient.dto.request.RequestIngredientDto;
+import com.kusher.kusher_in_korea.ingredient.dto.response.CategoryDto;
+import com.kusher.kusher_in_korea.ingredient.dto.response.IngredientDto;
 import com.kusher.kusher_in_korea.ingredient.service.IngredientService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -14,27 +19,74 @@ public class IngredientController {
 
     // 전체 식재료 조회
     @GetMapping("/api/ingredient")
-    public void findAllIngredients() {
-        ingredientService.findAllIngredients();
+    public ResponseEntity<List<IngredientDto>> findAllIngredients() {
+        List<IngredientDto> ingredients = ingredientService.findAllIngredients();
+        return ResponseEntity.ok(ingredients);
     }
 
     // 특정 식재료 조회
     @GetMapping("/api/ingredient/{ingredientId}")
-    public void findIngredient(@PathVariable("ingredientId") Long ingredientId) {
-        ingredientService.findIngredient(ingredientId);
+    public ResponseEntity<IngredientDto> findIngredient(@PathVariable("ingredientId") Long ingredientId) {
+        IngredientDto ingredient = ingredientService.findIngredient(ingredientId);
+        return ResponseEntity.ok(ingredient);
     }
 
     // 전체 카테고리 조회
     @GetMapping("/api/category")
-    public void findAllCategories() {
-        ingredientService.findAllCategories();
+    public ResponseEntity<List<CategoryDto>> findAllCategories() {
+        List<CategoryDto> categories = ingredientService.findAllCategories();
+        return ResponseEntity.ok(categories);
     }
 
     // 특정 카테고리에 속한 식재료 조회
-    @GetMapping("/api/category/{categoryName}")
-    public void findIngredientsByCategory(@PathVariable("categoryName") String categoryName) {
-        ingredientService.findIngredientsByCategory(categoryName);
+    @GetMapping("/api/category/{categoryName}/ingredient")
+    public ResponseEntity<List<IngredientDto>> findIngredientsByCategory(@PathVariable("categoryName") String categoryName) {
+        List<IngredientDto> ingredients = ingredientService.findIngredientsByCategory(categoryName);
+        return ResponseEntity.ok(ingredients);
     }
 
-    // 아래부터는 관리용 메서드에 대한 컨트롤러 (추후 구현)
+    // 아래부터는 서버에서 사용하는 관리용 메서드에 대한 Controller
+
+    // 식재료 추가
+    @PostMapping("/api/ingredient")
+    public ResponseEntity<Long> addIngredient(RequestIngredientDto requestIngredientDto) {
+        Long id = ingredientService.addIngredient(requestIngredientDto);
+        return ResponseEntity.ok(id);
+    }
+
+    // 식재료 수정
+    @PutMapping("/api/ingredient/{ingredientId}")
+    public ResponseEntity<Long> updateIngredient(@PathVariable("ingredientId") Long ingredientId, RequestIngredientDto requestIngredientDto) {
+        Long id = ingredientService.updateIngredient(ingredientId, requestIngredientDto);
+        return ResponseEntity.ok(id);
+    }
+
+    // 식재료 삭제
+    @DeleteMapping("/api/ingredient/{ingredientId}")
+    public ResponseEntity<Void> deleteIngredient(@PathVariable("ingredientId") Long ingredientId) {
+       ingredientService.deleteIngredient(ingredientId);
+         return ResponseEntity.ok().build();
+    }
+
+    // 카테고리 추가
+    @PostMapping("/api/category")
+    public ResponseEntity<Long> addCategory(@RequestBody RequestCategoryDto requestCategoryDto) {
+        Long id = ingredientService.addCategory(requestCategoryDto.getCategoryName());
+        return ResponseEntity.ok(id);
+    }
+
+    // 카테고리 수정
+    @PutMapping("/api/category/{categoryId}")
+    public ResponseEntity<Long> updateCategory(@PathVariable("categoryId") Long categoryId, @RequestBody RequestCategoryDto requestCategoryDto) {
+        Long id = ingredientService.updateCategory(categoryId, requestCategoryDto.getCategoryName());
+        return ResponseEntity.ok(id);
+    }
+
+    // 카테고리 삭제
+    @DeleteMapping("/api/category/{categoryId}")
+    public ResponseEntity<Void> deleteCategory(@PathVariable("categoryId") Long categoryId) {
+        ingredientService.deleteCategory(categoryId);
+        return ResponseEntity.ok().build();
+    }
+
 }
--- src/main/java/com/kusher/kusher_in_korea/ingredient/service/IngredientService.java
+++ src/main/java/com/kusher/kusher_in_korea/ingredient/service/IngredientService.java
@@ -2,13 +2,16 @@ package com.kusher.kusher_in_korea.ingredient.service;
 
 import com.kusher.kusher_in_korea.ingredient.domain.Category;
 import com.kusher.kusher_in_korea.ingredient.domain.Ingredient;
-import com.kusher.kusher_in_korea.ingredient.dto.createIngredientDto;
+import com.kusher.kusher_in_korea.ingredient.dto.request.RequestIngredientDto;
+import com.kusher.kusher_in_korea.ingredient.dto.response.CategoryDto;
+import com.kusher.kusher_in_korea.ingredient.dto.response.IngredientDto;
 import com.kusher.kusher_in_korea.ingredient.repository.CategoryRepository;
 import com.kusher.kusher_in_korea.ingredient.repository.IngredientRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -18,34 +21,43 @@ public class IngredientService { // 식재료 및 카테고리를 제어한다.
     private final CategoryRepository categoryRepository;
 
     // 전체 식재료 조회
-    public List<Ingredient> findAllIngredients() {
-        return ingredientRepository.findAll();
+    public List<IngredientDto> findAllIngredients() {
+        List<Ingredient> ingredients = ingredientRepository.findAll();
+        return ingredients.stream()
+                .map(IngredientDto::new)
+                .collect(Collectors.toList());
     }
 
     // 클라이언트가 클릭했을 때를 대비한 특정 식재료 조회
-    public Ingredient findIngredient(Long ingredientId) {
-        return ingredientRepository.findById(ingredientId).orElseThrow(() -> new IllegalStateException("존재하지 않는 식재료입니다."));
+    public IngredientDto findIngredient(Long ingredientId) {
+        return new IngredientDto(ingredientRepository.findById(ingredientId).orElseThrow(() -> new IllegalStateException("존재하지 않는 식재료입니다.")));
     }
 
     // 전체 카테고리 조회
-    public List<Category> findAllCategories() {
-        return categoryRepository.findAll();
+    public List<CategoryDto> findAllCategories() {
+        List<Category> categories = categoryRepository.findAll();
+        return categories.stream()
+                .map(CategoryDto::new)
+                .collect(Collectors.toList());
     }
 
     // 특정 카테고리에 속한 식재료 조회
-    public List<Ingredient> findIngredientsByCategory(String categoryName) {
+    public List<IngredientDto> findIngredientsByCategory(String categoryName) {
         Category category = categoryRepository.findByName(categoryName).orElseThrow(() -> new IllegalStateException("존재하지 않는 카테고리입니다."));
-        return ingredientRepository.findByCategoryId(category.getId());
+        List<Ingredient> ingredients = ingredientRepository.findByCategoryId(category.getId());
+        return ingredients.stream()
+                .map(IngredientDto::new)
+                .collect(Collectors.toList());
     }
 
     /**
      * 아래부터는 서버에서 사용하기 위한 관리용 메서드
      */
     // 식재료 추가
-    public Long addIngredient(createIngredientDto createIngredientDto) {
-        validateDuplicateIngredient(createIngredientDto.getIngredientName());
-        Category category = categoryRepository.findByName(createIngredientDto.getIngredientCategory()).orElseThrow(() -> new IllegalStateException("존재하지 않는 카테고리입니다."));
-        Ingredient ingredient = Ingredient.createIngredient(createIngredientDto.getIngredientName(), createIngredientDto.getIngredientPrice(), category);
+    public Long addIngredient(RequestIngredientDto requestIngredientDto) {
+        validateDuplicateIngredient(requestIngredientDto.getIngredientName());
+        Category category = categoryRepository.findByName(requestIngredientDto.getIngredientCategory()).orElseThrow(() -> new IllegalStateException("존재하지 않는 카테고리입니다."));
+        Ingredient ingredient = Ingredient.createIngredient(requestIngredientDto.getIngredientName(), requestIngredientDto.getIngredientPrice(), category);
         ingredientRepository.save(ingredient);
         return ingredient.getId();
     }
@@ -58,21 +70,22 @@ public class IngredientService { // 식재료 및 카테고리를 제어한다.
     }
 
     // 식재료 수정
-    public Long updateIngredient(Long ingredientId, String ingredientName, Long categoryId) {
+    public Long updateIngredient(Long ingredientId, RequestIngredientDto requestIngredientDto) {
         Ingredient ingredient = ingredientRepository.findById(ingredientId).orElseThrow(() -> new IllegalStateException("존재하지 않는 식재료입니다."));
-        ingredient.setName(ingredientName);
-        ingredient.setCategory(categoryRepository.findById(categoryId).orElseThrow(() -> new IllegalStateException("존재하지 않는 카테고리입니다.")));
+        Category category = categoryRepository.findByName(requestIngredientDto.getIngredientCategory()).orElseThrow(() -> new IllegalStateException("존재하지 않는 카테고리입니다."));
+        ingredient.updateIngredient(requestIngredientDto.getIngredientName(), requestIngredientDto.getIngredientPrice(), category);
         return ingredient.getId();
     }
 
     // 식재료 삭제
-    private void deleteIngredient(Long ingredientId) {
+    public void deleteIngredient(Long ingredientId) {
         ingredientRepository.deleteById(ingredientId);
     }
 
     // 카테고리 추가
-    public Long addCategory(Category category) {
-        validateDuplicateCategory(category.getName());
+    public Long addCategory(String categoryName) {
+        validateDuplicateCategory(categoryName);
+        Category category = Category.createCategory(categoryName);
         categoryRepository.save(category);
         return category.getId();
     }
