959e2037cceb1817a9e11aaebb5f4fc1798ff3fd
CHAE <chaso1495@naver.com>
2023-08-07 22:41:13 +0900

    [refactor] #30 IngredientController 리팩토링

--- src/main/java/com/kusher/kusher_in_korea/ingredient/controller/IngredientController.java
+++ src/main/java/com/kusher/kusher_in_korea/ingredient/controller/IngredientController.java
@@ -7,8 +7,9 @@ import com.kusher.kusher_in_korea.ingredient.dto.request.RequestIngredientDto;
 import com.kusher.kusher_in_korea.ingredient.dto.response.CategoryDto;
 import com.kusher.kusher_in_korea.ingredient.dto.response.IngredientDto;
 import com.kusher.kusher_in_korea.ingredient.service.IngredientService;
+import com.kusher.kusher_in_korea.util.api.ApiResponse;
+import com.kusher.kusher_in_korea.util.exception.ResponseCode;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
@@ -21,77 +22,77 @@ public class IngredientController {
     private final IngredientService ingredientService;
     private final ImageUploadService imageUploadService;
 
-    // 전체 식재료 조회
+    // 전체 식재료 조회 (페이징 필요)
     @GetMapping("/api/ingredient")
-    public ResponseEntity<List<IngredientDto>> findAllIngredients() {
+    public ApiResponse<List<IngredientDto>> findAllIngredients() {
         List<IngredientDto> ingredients = ingredientService.findAllIngredients();
-        return ResponseEntity.ok(ingredients);
+        return ApiResponse.success(ingredients, ResponseCode.INGREDIENT_READ_SUCCESS.getMessage());
     }
 
     // 특정 식재료 조회
     @GetMapping("/api/ingredient/{ingredientId}")
-    public ResponseEntity<IngredientDto> findIngredient(@PathVariable("ingredientId") Long ingredientId) {
+    public ApiResponse<IngredientDto> findIngredient(@PathVariable("ingredientId") Long ingredientId) {
         IngredientDto ingredient = ingredientService.findIngredient(ingredientId);
-        return ResponseEntity.ok(ingredient);
+        return ApiResponse.success(ingredient, ResponseCode.INGREDIENT_READ_SUCCESS.getMessage());
     }
 
     // 전체 카테고리 조회
     @GetMapping("/api/category")
-    public ResponseEntity<List<CategoryDto>> findAllCategories() {
+    public ApiResponse<List<CategoryDto>> findAllCategories() {
         List<CategoryDto> categories = ingredientService.findAllCategories();
-        return ResponseEntity.ok(categories);
+        return ApiResponse.success(categories, ResponseCode.CATEGORY_READ_SUCCESS.getMessage());
     }
 
     // 특정 카테고리에 속한 식재료 조회
     @GetMapping("/api/category/{categoryName}/ingredient")
-    public ResponseEntity<List<IngredientDto>> findIngredientsByCategory(@PathVariable("categoryName") String categoryName) {
+    public ApiResponse<List<IngredientDto>> findIngredientsByCategory(@PathVariable("categoryName") String categoryName) {
         List<IngredientDto> ingredients = ingredientService.findIngredientsByCategory(categoryName);
-        return ResponseEntity.ok(ingredients);
+        return ApiResponse.success(ingredients, ResponseCode.INGREDIENT_READ_SUCCESS.getMessage());
     }
 
     // 아래부터는 서버에서 사용하는 관리용 메서드에 대한 Controller
 
     // 식재료 추가
     @PostMapping("/api/ingredient")
-    public ResponseEntity<Long> addIngredient(CreateIngredientDto ingredientDto) throws IOException {
+    public ApiResponse<Long> addIngredient(CreateIngredientDto ingredientDto) throws IOException {
         String imageUrl = imageUploadService.uploadImage(ingredientDto.getIngredientImage());
         Long id = ingredientService.addIngredient(ingredientDto, imageUrl);
-        return ResponseEntity.ok(id);
+        return ApiResponse.success(id, ResponseCode.INGREDIENT_CREATE_SUCCESS.getMessage());
     }
 
     // 식재료 수정
     @PutMapping("/api/ingredient/{ingredientId}")
-    public ResponseEntity<Long> updateIngredient(@PathVariable("ingredientId") Long ingredientId, RequestIngredientDto requestIngredientDto) {
+    public ApiResponse<Long> updateIngredient(@PathVariable("ingredientId") Long ingredientId, RequestIngredientDto requestIngredientDto) {
         Long id = ingredientService.updateIngredient(ingredientId, requestIngredientDto);
-        return ResponseEntity.ok(id);
+        return ApiResponse.success(id, ResponseCode.INGREDIENT_UPDATE_SUCCESS.getMessage());
     }
 
     // 식재료 삭제
     @DeleteMapping("/api/ingredient/{ingredientId}")
-    public ResponseEntity<Void> deleteIngredient(@PathVariable("ingredientId") Long ingredientId) {
+    public ApiResponse<Void> deleteIngredient(@PathVariable("ingredientId") Long ingredientId) {
        ingredientService.deleteIngredient(ingredientId);
-         return ResponseEntity.ok().build();
+         return ApiResponse.success(null, ResponseCode.INGREDIENT_DELETE_SUCCESS.getMessage());
     }
 
     // 카테고리 추가
     @PostMapping("/api/category")
-    public ResponseEntity<Long> addCategory(@RequestBody RequestCategoryDto requestCategoryDto) {
+    public ApiResponse<Long> addCategory(@RequestBody RequestCategoryDto requestCategoryDto) {
         Long id = ingredientService.addCategory(requestCategoryDto.getCategoryName());
-        return ResponseEntity.ok(id);
+        return ApiResponse.success(id, ResponseCode.CATEGORY_CREATE_SUCCESS.getMessage());
     }
 
     // 카테고리 수정
     @PutMapping("/api/category/{categoryId}")
-    public ResponseEntity<Long> updateCategory(@PathVariable("categoryId") Long categoryId, @RequestBody RequestCategoryDto requestCategoryDto) {
+    public ApiResponse<Long> updateCategory(@PathVariable("categoryId") Long categoryId, @RequestBody RequestCategoryDto requestCategoryDto) {
         Long id = ingredientService.updateCategory(categoryId, requestCategoryDto.getCategoryName());
-        return ResponseEntity.ok(id);
+        return ApiResponse.success(id, ResponseCode.CATEGORY_UPDATE_SUCCESS.getMessage());
     }
 
     // 카테고리 삭제
     @DeleteMapping("/api/category/{categoryId}")
-    public ResponseEntity<Void> deleteCategory(@PathVariable("categoryId") Long categoryId) {
+    public ApiResponse<Void> deleteCategory(@PathVariable("categoryId") Long categoryId) {
         ingredientService.deleteCategory(categoryId);
-        return ResponseEntity.ok().build();
+        return ApiResponse.success(null, ResponseCode.CATEGORY_DELETE_SUCCESS.getMessage());
     }
 
 }
