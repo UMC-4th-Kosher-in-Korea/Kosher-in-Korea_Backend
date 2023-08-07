a2f277cb79394e55a1f2d01dc77a97bb655db14b
CHAE <chaso1495@naver.com>
2023-08-07 16:54:40 +0900

    [fix] #28 feedback 생성메서드 추가 및 dto 수정

--- src/main/java/com/kusher/kusher_in_korea/reviewfeedback/dto/CreateFeedbackDto.java
+++ src/main/java/com/kusher/kusher_in_korea/reviewfeedback/dto/CreateFeedbackDto.java
@@ -8,5 +8,4 @@ import lombok.NoArgsConstructor;
 public class CreateFeedbackDto { // 앱 개발자들에게 피드백을 전송할 목적, 그러므로 FeedbackDto는 필요없음
     private Long userId; // 유저번호
     private String feedbackContent; // 피드백 내용
-    private String feedbackTime; // 피드백 작성일시 -> LocalDateTime으로 변경될 수도 있음
 }
