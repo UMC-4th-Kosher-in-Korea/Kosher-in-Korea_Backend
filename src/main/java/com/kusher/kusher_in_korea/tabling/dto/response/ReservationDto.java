7b289d48f7d8eba361da33554f03627a315b8dcf
CHAE <chaso1495@naver.com>
2023-08-14 16:50:35 +0900

    [fix] #43 예약일시 속성 String에서 LocalDate/Time으로 수정

--- src/main/java/com/kusher/kusher_in_korea/tabling/dto/response/ReservationDto.java
+++ src/main/java/com/kusher/kusher_in_korea/tabling/dto/response/ReservationDto.java
@@ -4,14 +4,17 @@ import com.kusher.kusher_in_korea.tabling.domain.Reservation;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 @Data
 @NoArgsConstructor
 public class ReservationDto { // 예약 응답
     private Long id; // 예약번호
     private Long userId; // 예약자(유저번호)
     private Long restaurantId; // 식당(식당번호)
-    private String reservationDate; // 예약날짜
-    private String reservationTime; // 예약시간
+    private LocalDate reservationDate; // 예약날짜
+    private LocalTime reservationTime; // 예약시간
     private int numberOfPeople; // 예약인원
     private String status; // 예약상태
 
@@ -19,8 +22,8 @@ public class ReservationDto { // 예약 응답
         this.id = reservation.getId();
         this.userId = reservation.getUser().getId();
         this.restaurantId = reservation.getRestaurant().getId();
-        this.reservationDate = reservation.getReservationDate().toString();
-        this.reservationTime = reservation.getReservationTime().toString();
+        this.reservationDate = reservation.getReservationDate();
+        this.reservationTime = reservation.getReservationTime();
         this.numberOfPeople = reservation.getNumberOfPeople();
         this.status = reservation.getStatus();
     }
